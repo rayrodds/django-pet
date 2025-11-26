2988357011fe6341a5632b8c3d948683436ef9e8
Rayan Rodrigues <rayan.estudo@outlook.com>
2025-11-26 14:36:17 -0300

    sei lça

--- setup/settings.py
+++ setup/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'root',
-        'PASSWORD': 'Meufilho@2233',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
